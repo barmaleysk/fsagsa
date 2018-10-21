c5710f3875e1214f1724e82cbb28bfdf6b0ff4a5
SonyLo <30892016+SonyLo@users.noreply.github.com>
2018-10-21 15:47:23 +0300

    Delete config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    TOKEN: process.env.TELEGRAM_TOKEN || '550257961:AAFUF2xgKx4ZB2GySlT99HfhcQe2l6qQ5bo',
-    DB_URL:'mongodb://admin:admin@ds233748.mlab.com:33748/tbot',
-    URL_ngrok: process.env.APP_URL ||  'https://mighty-anchorage-91287.herokuapp.com:443',
-    Port: process.env.PORT || 443
-}
-
