7882b56368ad09f339797776a98c081d8b923932
barmaleysk <43924454+barmaleysk@users.noreply.github.com>
2018-10-24 19:41:36 +0400

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {		
+	TOKEN: process.env.TELEGRAM_TOKEN || '586419223:AAEFNDyEj1BO6lDHjP-CyDbDxyOtu0LEUjI',		
+	DB_URL:'mongodb://admin:admin@ds233748.mlab.com:33748/tbot',		
+	URL_ngrok: process.env.APP_URL || 'https://testgive.herokuapp.com:443',		
+	Port: process.env.PORT || 443		
+	}		
+
